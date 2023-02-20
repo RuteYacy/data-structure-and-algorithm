0746a2ae43046fbec0eba31a16622d29be7563f4
Rute Souza <rutesouza6801@gmail.com>
2023-02-20 17:56:49 -0300

    first commit

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -1,99 +1,58 @@
-from collections import defaultdict
-
 class Graph:
     def __init__(self):
-        self.gDict = {}
-        
-    def addVertex(self, vertex):
-        if vertex not in self.gDict.keys():
-            self.gDict[vertex] = []
+        self.adjacency_list = {}
+    
+    def add_vertex(self, vertex):
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
             return True
         return False
     
-    def addEdge(self, vertex1, vertex2):
-        if vertex1 in self.gDict.keys() and vertex2 in self.gDict.keys():
-            self.gDict[vertex1].append(vertex2)
-            self.gDict[vertex2].append(vertex1)
+    def print_graph(self):
+        for vertex in self.adjacency_list:
+            print(vertex,":",self.adjacency_list[vertex])
+    
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
             return True
         return False
     
-    def printGraph(self):
-        for vertex in self.gDict:
-            print(vertex, ":", self.gDict[vertex])
-    
-    def removeEdge(self, vertex1, vertex2):
-        if vertex1 in self.gDict.keys() and vertex2 in self.gDict.keys():
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
             try:
-                self.gDict[vertex1].remove(vertex2)
-                self.gDict[vertex2].remove(vertex1)
+                self.adjacency_list[vertex1].remove(vertex2)
+                self.adjacency_list[vertex2].remove(vertex1)
             except ValueError:
                 pass
             return True
         return False
     
-    def removeVertex(self, vertex):
-        if vertex in self.gDict.keys():
-            for other_vertex in self.gDict[vertex]:
-                self.gDict[other_vertex].remove(vertex)
-            del self.gDict[vertex]
+    def remove_vertex(self, vertex):
+        if vertex in self.adjacency_list.keys():
+            for other_vertex in self.adjacency_list[vertex]:
+                self.adjacency_list[other_vertex].remove(vertex)
+            del self.adjacency_list[vertex]
             return True
         return False
- 
-class Graph1:
-    def __init__(self, numberOfVertex):
-        self.graph = defaultdict(list)
-        self.numberOfVertex = numberOfVertex
-        
-    def addEdge(self, vertex, edge):
-        self.graph[vertex].append(edge)
-        
-    def dfs(self, vertex):
-        visited = [vertex]
-        stack = [vertex]
-        while stack:
-            popVertex = stack.pop()
-            print(popVertex)
-            for adjVertex in self.gDict[popVertex]:
-                if adjVertex not in visited:
-                    stack.append(adjVertex)
-                     
-    def bfs(self, vertex):
-        visited = [vertex]
-        queue = [vertex]
-        while queue:
-            deVertex = queue.pop(0)
-            print(deVertex)
-            for adjVertex in self.gDict[deVertex]:
-                if adjVertex not in visited:
-                    queue.append(adjVertex)
-                    
-    def topologicalSortUtil(self, v, visited, stack):
-        visited.append(v)
-        for i in self.graph[v]:
-            if i not in visited:
-                self.topologicalSortUtil(i, visited, stack)
-        stack.insert(0, v)
-        
-    def topologicalSort(self):
-        visited = []
-        stack = []
-        for k in list(self.graph):
-            if k not in visited:
-                self.topologicalSortUtil(k, visited, stack)
-        print(stack)
+
+my_graph = Graph()
+my_graph.add_vertex("A")
+my_graph.add_vertex("B")
+my_graph.add_vertex("C")
+my_graph.add_vertex("D")
+my_graph.add_edge("A", "B")
+my_graph.add_edge("A", "C")
+my_graph.add_edge("A", "D")
+my_graph.add_edge("B", "C")
+my_graph.add_edge("C", "D")
+my_graph.print_graph()
+my_graph.remove_vertex("A")
+print("After remove..")
+my_graph.print_graph()
+
+  
     
-            
-graph = Graph()
-graph.addVertex("A")
-graph.addVertex("B")
-graph.addVertex("C")
-graph.addVertex("D")
-graph.addEdge("A", "B")
-graph.addEdge("A", "C")
-graph.addEdge("A", "D")
-graph.addEdge("B", "C")
-graph.addEdge("C", "D")
-graph.printGraph()
-graph.removeVertex("D")
-print("-------------")
-graph.dfs("A")
+
+
